3b1a521576cc4a9387fc3083a2a6b895e762ebff
KrishnaLikhith <76393325+KrishnaLikhith@users.noreply.github.com>
2021-03-16 10:32:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("My First Console Project");
+var name1= "Krishna" ; 
+var name2 = "Likhith" ;
+var fullname = name1.concat(name2);
+console.log(fullname);
+var x = 5 ;
+console.log(x);
+var y = +23 ;
+console.log(y);
+var z = x + y ;
+console.log(z); 
+
